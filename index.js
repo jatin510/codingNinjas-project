89e318b07ef8b5d500bbf40b625a15fd092c9af4
Jagdish <jatin6972@gmail.com>
2020-05-22 00:57:20 +0530

    setting up view engine

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const port = process.env.PORT || 8000;
 const app = express();
 
+//set up view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 // middleware
 // express router
 app.use("/", require("./routes"));
