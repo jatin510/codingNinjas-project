2437653b42436ccc1da460e2bdc4fc87e6a4d347
Jagdish <jatin6972@gmail.com>
2020-05-22 22:59:22 +0530

    setup initial controller and routes for user signIn and signUp

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,13 @@
 module.exports.profile = (req, res) => {
   res.end("<h1>user profile</h1>");
 };
+
+// redner sign up page
+module.exports.signUp = (req, res) => {
+  return res.render("user_sign_up", { title: "Codial | Sign Up" });
+};
+
+// render sing in page
+module.exports.signIn = (req, res) => {
+  return res.render("user_sign_in", { title: "Codial | Sign In" });
+};
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersController = require("../controllers/users_controller");
 const postsController = require("../controllers/posts_controller");
 router.get("/profile", usersController.profile);
-router.get("/post", postsController.posts);
+router.get("/sign-up", usersController.signUp);
+router.get("/sign-in", usersController.signIn);
 
 module.exports = router;
