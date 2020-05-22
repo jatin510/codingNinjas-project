574cf203c6bd6893da8a6b2d826fc9d3313d82d9
Jagdish <jatin6972@gmail.com>
2020-05-22 18:00:49 +0530

    set up views and layouts

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const port = process.env.PORT || 8000;
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+// setting up layouts
+app.use(expressLayouts);
+// extract style and script from sub pages
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.use(express.static("./assets"));
 
 //set up view engine
 app.set("view engine", "ejs");
