d0801b0eccfe14effcf17afef944a9500f7e61eb
Jagdish <jatin6972@gmail.com>
2020-05-29 00:42:07 +0530

    home view completed for showing comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -10,6 +10,12 @@ module.exports.home = function (req, res) {
   // populate the user of each post
   Post.find({})
     .populate("user")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+      },
+    })
     .exec((err, posts) => {
       if (err) return console.log("error in home controller");
       return res.render("home", { title: "Codeial | Home", posts });
